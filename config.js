cb096efba7b6ad9993ecfc32644cd8056b64d653
wit03212 <wit03212@gmail.com>
2026-05-18 15:18:31 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+  API_URL:
+    "https://docs.google.com/spreadsheets/d/15T3HJVZjVL89gGg6l7uSvQAwodlK7avoSc57stolF5E/edit?usp=sharing",
+
+  ADMIN_PASSWORD:
+    "12345678"
+
+};
