38793e36b1f86ec68fe21d407751f53705bbda2d
wit03212 <wit03212@gmail.com>
2026-05-19 00:16:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,40 +2,64 @@ const form = document.getElementById("form");
 const result = document.getElementById("result");
 
 form.addEventListener("submit", async (e) => {
+
   e.preventDefault();
 
+  const submitBtn =
+    document.querySelector(".submit-btn");
+
+  submitBtn.innerHTML =
+    "กำลังบันทึก...";
+
   const data = {
+
     empId: empId.value,
     fullName: fullName.value,
     department: department.value,
+
     oldDate: oldDate.value,
     oldShift: oldShift.value,
+
     newDate: newDate.value,
     newShift: newShift.value,
-    reason: reason.value
-  };
 
-  result.innerHTML = "กำลังส่งข้อมูล...";
+    reason: reason.value
+
+  };
 
   try {
 
     await fetch(CONFIG.API_URL, {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json"
+
+      method:"POST",
+
+      mode:"no-cors",
+
+      headers:{
+        "Content-Type":"application/json"
       },
-      body: JSON.stringify(data)
+
+      body:JSON.stringify(data)
+
     });
 
-    result.innerHTML = "ส่งข้อมูลเรียบร้อยแล้ว";
+    result.innerHTML =
+      "ส่งข้อมูลเรียบร้อยแล้ว";
+
+    submitBtn.innerHTML =
+      "บันทึกข้อมูล";
 
     form.reset();
 
-  } catch (err) {
+  } catch(err){
 
     console.log(err);
-    result.innerHTML = "เกิดข้อผิดพลาด";
 
+    result.innerHTML =
+      "เกิดข้อผิดพลาด";
+
+    submitBtn.innerHTML =
+      "บันทึกข้อมูล";
   }
+
 });
