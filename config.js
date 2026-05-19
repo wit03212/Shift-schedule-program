6e07941dce9ad07eb75576ef7d9ee9845716e1d6
wit03212 <wit03212@gmail.com>
2026-05-19 20:11:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
   API_URL:
-  "https://script.google.com/macros/s/AKfycbyO95BjkoUGbx0INfLREsvZXNXKqOq8TNMDIGnDaxuc3w04Lb71n1g6k_L7VKWejwgv/exec"
+  "https://script.google.com/macros/s/AKfycbyO95BjkoUGbx0INfLREsvZXNXKqOq8TNMDIGnDaxuc3w04Lb71n1g6k_L7VKWejwgv/exec",
   ADMIN_PASSWORD: "12345678"
 };
